39f0afb1957b00a7ff2153b94070c61de6913007
Sam Needham <src.needham@gmail.com>
2021-08-03 15:43:01 +0100

    Adds forum posts and threads views

--- app/models/forum_post.rb
+++ app/models/forum_post.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: forum_posts
+#
+#  id              :integer          not null, primary key
+#  body            :text
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  forum_thread_id :integer
+#  user_id         :integer
+#
 class ForumPost < ApplicationRecord
   belongs_to :forum_thread
   belongs_to :user
