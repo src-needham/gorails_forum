2e7f0f33963ca5211105db50105419ca8b4eb2ea
Sam Needham <src.needham@gmail.com>
2021-08-03 15:42:28 +0100

    Updates forum_threads_controller

--- app/controllers/forum_threads_controller.rb
+++ app/controllers/forum_threads_controller.rb
@@ -1,12 +1,14 @@
-class ForumThreadsController < ApplicationContoller
+class ForumThreadsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_forum_thread, except: [:index, :new, :create]
 
   def index
-    @forum_threads = ForumThread.all
+    @q = ForumThread.search(params[:q])
+    @forum_threads = @q.result(distinct: true)
   end
 
   def show
+    @forum_post = ForumPost.new
   end
 
   def new
@@ -15,11 +17,23 @@ class ForumThreadsController < ApplicationContoller
   end
 
   def create
+    @forum_thread = current_user.forum_threads.new forum_thread_params
+    @forum_thread.forum_posts.first.user_id = current_user.id
+
+    if @forum_thread.save
+      redirect_to @forum_thread
+    else
+      render action: :new
+    end
   end
 
   private
 
     def set_forum_thread
       @forum_thread = ForumThread.find(params[:id])
+    end
+
+    def forum_thread_params
+      params.require(:forum_thread).permit(:subject, forum_posts_attributes: [:body])
     end
 end
