3ef04faf3d594e6fec17c3c7fa171ab7c88e7e29
Sam Needham <src.needham@gmail.com>
2021-08-04 14:56:31 +0100

    Adds moar columns to user table

--- app/models/forum_thread.rb
+++ app/models/forum_thread.rb
@@ -11,4 +11,9 @@
 class ForumThread < ApplicationRecord
   belongs_to :user
   has_many :forum_posts
+
+  accepts_nested_attributes_for :forum_posts
+
+  validates :subject, presence: true
+  validates_associated :forum_posts
 end
