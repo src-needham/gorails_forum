7b351c74953cc5d9ef7e2e5d029cd6129a338098
Sam Needham <src.needham@gmail.com>
2021-08-05 14:33:01 +0100

    Adds deleted at to schema

--- app/models/forum_thread.rb
+++ app/models/forum_thread.rb
@@ -16,30 +16,4 @@ class ForumThread < ApplicationRecord
 
   validates :subject, presence: true
   validates_associated :forum_posts
-
-  def self.assign_from_row(row)
-    user = User.where(email: row[:email]).first_or_initialize
-    user.assign_attributes row.to_hash.slice(:first_name, :last_name)
-    user
-  end
-
-  def self.to_csv
-    attributes = %w{id email name}
-
-    CSV.generate(headers: true) do |csv|
-      csv << attributes
-
-      all.each do |user|
-        csv << attributes.map{ |attr| user.send(attr) }
-      end
-    end
-  end
-
-  def name
-    if deleted_at?
-      "Deleted User"
-    else
-      "#{first_name} #{last_name}"
-    end
-  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -24,4 +24,30 @@ class User < ApplicationRecord
 
   has_many :forum_threads
   has_many :forum_posts
+
+  def self.assign_from_row(row)
+    user = User.where(email: row[:email]).first_or_initialize
+    user.assign_attributes row.to_hash.slice(:first_name, :last_name)
+    user
+  end
+
+  def self.to_csv
+    attributes = %w{id email name}
+
+    CSV.generate(headers: true) do |csv|
+      csv << attributes
+
+      all.each do |user|
+        csv << attributes.map{ |attr| user.send(attr) }
+      end
+    end
+  end
+
+  def name
+    if deleted_at?
+      "Deleted User"
+    else
+      "#{first_name} #{last_name}"
+    end
+  end
 end
