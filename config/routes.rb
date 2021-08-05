9e87d5777c8ffb44f23bdbd1aa49d36c46a55681
Sam Needham <src.needham@gmail.com>
2021-08-05 18:30:08 +0100

    Updates routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,13 @@ Rails.application.routes.draw do
   resources :forum_threads do
     resources :forum_posts, module: :forum_threads
   end
+
+  resources :users do
+    collection do
+      post :import
+    end
+  end
+
+  root to: "forum_threads#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
